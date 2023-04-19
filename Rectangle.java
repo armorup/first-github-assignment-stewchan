e0b0e7a4488657d98289e3ef22a64dc11957d67c
Stewart Chan <stew.chan@gmail.com>
2023-04-19 08:19:32 -0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    private int width;
+    private int height;
+    
+    public Rectangle() {
+    }
+    
+    public int getArea() {
+        return width * height;
+    }
+}
